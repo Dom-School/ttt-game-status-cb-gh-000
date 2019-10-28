bf9387e8af04124de864ec27d8edecf4334d4283
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-10-28 12:04:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,8 @@ def won?(board)
   WIN_COMBINATIONS.each do |combo|
     puts "#{board[combo[0]]} == X and #{board[combo[1]]} == X and #{board[combo[2]]} == X"
     if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") || (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")
-        puts "#{combo}"
+        return true
     end
   end
+  return false
 end
